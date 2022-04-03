53fc0724a6c34b03a0e57ce6803b39266c5929a1
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-04-04 00:14:36 +0200

    feat: :sparkles: Implemented code highlighting blog

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export function links() {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "A Frontend Samurai's Dev Blog",
   viewport: "width=device-width,initial-scale=1",
 });
 
