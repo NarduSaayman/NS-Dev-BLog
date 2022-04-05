002c37cb97b4853b1afb3e98c61848aaaffd7669
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-04-05 17:46:24 +0200

    fix: :bug: Added animate presence to root and outlets

--- app/components/blog-component.tsx
+++ app/components/blog-component.tsx
@@ -1,3 +1,5 @@
+import FrontendSamurai from "./frontend-samurai";
+
 type BlogComponentProps = {
   heading: string;
   description: string;
@@ -11,11 +13,20 @@ export default function BlogComponent(props: BlogComponentProps) {
     bg-gradient-to-bl from-green-300 via-blue-500 to-purple-600 
     hover:bg-gradient-to-br hover:from-pink-500 hover:via-red-500 hover:to-yellow-500"
     >
-      <h3 className="text-white group-hover:animate-bounce">
-        <span className="inline group-hover:hidden">{heading} is blue!</span>
-        <span className="hidden group-hover:inline">{heading} is red!</span>
-      </h3>
-      <p className="group-hover:animate-pulse">{description}</p>
+      <div className="flex flex-row">
+        <div>
+          <h3 className="text-white group-hover:animate-bounce">
+            <span className="inline group-hover:hidden">
+              {heading} is blue!
+            </span>
+            <span className="hidden group-hover:inline">{heading} is red!</span>
+          </h3>
+          <p className="group-hover:animate-pulse">{description}</p>
+        </div>
+        <div className="m-auto">
+          <FrontendSamurai width={"90"} height={"90"} />
+        </div>
+      </div>
     </div>
   );
 }
--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -2,7 +2,7 @@ import { Link, LoaderFunction, useLoaderData } from "remix";
 import * as whatIsMdx from "./what-is-mdx.mdx";
 import * as codeHighlighting from "./code-highlighting.mdx";
 import * as animations from "./animations.mdx";
-import { animate, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 function postFromModule(mod: any) {
   return {
@@ -24,37 +24,35 @@ export default function BlogIndex() {
   let direction = 1;
 
   return (
-    <motion.div
-      className="flex flex-col"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1 }}
-      key={"Blogs"}
-    >
-      <motion.div initial={{ y: 100 }} animate={{ y: 0 }} exit={{ y: 100 }}>
-        <div className="mx-auto -mt-20 rounded-lg shadow-xl px-10 bg-gradient-to-br from-pink-400 via-red-400 to-yellow-400 z-0 py-5 w-6/12 text-center mb-8">
+    <>
+      <motion.div
+        initial={{ y: 100 }}
+        animate={{ y: 0 }}
+        exit={{ y: -100, opacity: 0 }}
+      >
+        <div className="mx-auto -mt-20 rounded-lg shadow-xl px-10 bg-gradient-to-br from-pink-400 via-red-400 to-yellow-400 z-0 py-5 min-w-fit w-6/12 text-center mb-8">
           <h1 style={{ marginBottom: "0px" }}>Blogs</h1>
         </div>
       </motion.div>
       <div className="p-1">
         <ul className="flex flex-col gap-4">
-          {posts.map(
+          {posts?.map(
             (post: { slug: string; title: string; description: string }) => (
-              <Link className="no-underline" key={post.slug} to={post.slug}>
-                <motion.div
-                  initial={{
-                    opacity: 0,
-                    x: -500 * (direction *= -1),
-                  }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{
-                    type: "spring",
-                    stiffness: 150,
-                    damping: 15,
-                  }}
-                  key={post.slug}
-                >
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  x: -500 * (direction *= -1),
+                }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ x: -200 * direction, opacity: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 150,
+                  damping: 15,
+                }}
+                key={post.slug}
+              >
+                <Link className="no-underline" key={post.slug} to={post.slug}>
                   <li
                     className="transition ease-in-out rounded-xl shadow-xl hover:shadow-xl hover:scale-105 pt-1 pb-10 px-5 
                 bg-gradient-to-bl from-green-200 via-blue-200 to-purple-200
@@ -72,6 +70,7 @@ export default function BlogIndex() {
                         x: { type: "spring", duration: 0.8, delay: 0.2 },
                         opacity: { delay: 0.2 },
                       }}
+                      key={post.slug}
                     >
                       <h3 className="mt-1 hover:underline">{post.title}</h3>
                     </motion.div>
@@ -79,12 +78,12 @@ export default function BlogIndex() {
                       <p className="m-0 lg:m-0">{post.description}</p>
                     ) : null}
                   </li>
-                </motion.div>
-              </Link>
+                </Link>
+              </motion.div>
             )
           )}
         </ul>
       </div>
-    </motion.div>
+    </>
   );
 }
